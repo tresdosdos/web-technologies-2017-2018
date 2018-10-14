3492386b2f958c2f52255f60dcc701d39c9bacd6
tresdosdos <vitaliystrelok8@gmail.com>
2018-10-14 12:38:27 +0300

    delete redundant modules

--- lab7/src/app.module.ts
+++ lab7/src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MovieModule } from './movie';
 import { ConfigModule } from './config';
 
 @Module({
   imports: [MovieModule, ConfigModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
