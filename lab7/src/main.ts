96c6a7787a9c4c8f078074e3ef18730a7ebbb38c
tresdosdos <vitaliystrelok8@gmail.com>
2018-10-16 11:04:05 +0300

    deploy fix

--- lab7/src/main.ts
+++ lab7/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 2000;
+  app.enableCors();
   await app.listen(port);
 
   process.on('SIGINT', () => {
